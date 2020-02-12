9caad883c6fec58d45b550acea7a9fd30243c281
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-02-12 17:54:23 +0200

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -3,8 +3,10 @@
 const totalCategories = document.querySelectorAll(".item");
 console.log(`В списке ${totalCategories.length} категории.`);
 
- const categoriesArray = [...totalCategories]
- .map(categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`)
- .join ('\n');
- console.log(categoriesArray);
+const categoriesArray = [...totalCategories]
+  .map(
+    categories => `Категория: ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`
+  )
+  .join("\n");
+console.log(categoriesArray);
