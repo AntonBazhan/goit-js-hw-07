51308513a0db515c51f53f5beea376bed2fe17b6
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-02-12 17:51:23 +0200

    1

---
+++ task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const totalCategories = document.querySelectorAll(".item");
+console.log(`В списке ${totalCategories.length} категории.`);
+
+ const categoriesArray = [...totalCategories]
+ .map(categories => `Категория: ${categories.children[0].textContent}
+Количество элементов: ${categories.children[1].children.length}`)
+ .join ('\n');
+ console.log(categoriesArray);
