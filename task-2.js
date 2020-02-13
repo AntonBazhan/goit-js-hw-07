8294476a02ccaa234a11b5920a7f101d4f16940b
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-02-13 02:12:29 +0200

    2

--- task-2.js
+++ task-2.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы"
+];
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.appendChild(items);
+});
+
+console.log(foodIngredients);
