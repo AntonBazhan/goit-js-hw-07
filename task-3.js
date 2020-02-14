ff4d98612eafb77d6c739ffb7a0ae887143740f4
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-02-14 02:06:17 +0200

    3

--- task-3.js
+++ task-3.js
@@ -1,2 +1,34 @@
 "use strict";
 
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat"
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish"
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running"
+  }
+];
+
+let ulList = document.getElementById("gallery");
+ulList.setAttribute("style", "list-style-type:none; display: flex;");
+
+const urlImage = images.forEach(image => {
+  const li = document.createElement("li");
+  let imageFile = document.createElement("img");
+  imageFile.setAttribute(
+    "style",
+    "width: 200px; height: 150px; margin-right: 20px"
+  );
+  ulList.appendChild(li).insertAdjacentElement("beforeEnd", imageFile);
+  imageFile.src = image.url;
+  imageFile.alt = image.alt;
+});
