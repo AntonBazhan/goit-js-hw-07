7a63c1ebc414599ce8ece806549ce3a5815b728d
AntonBazhan <51802003+AntonBazhan@users.noreply.github.com>
2020-03-17 14:41:26 +0200

    4

--- task-4.js
+++ task-4.js
@@ -10,7 +10,6 @@ const button = {
   sub: document.querySelector("[data-action='increment']"),
   add: document.querySelector("[data-action='decrement']"),
   span: document.querySelector("#value"),
-  counter: document.querySelector("#counter ")
 };
 
 let counterValue = 0;
